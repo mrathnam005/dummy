2035a2bfbc03def2f77154bd425555043da0ce0a
Mani Rathnam <mrathnam005@gmail.com>
2019-05-27 11:48:18 +0530

    added star main file

--- eclipse-workspace/SamplePorject/src/com/collection/StarWar.java
+++ eclipse-workspace/SamplePorject/src/com/collection/StarWar.java
@@ -1,108 +1,36 @@
+package com.collection;
 
 
-  package com.collection;
-  
-  
-  import java.util.concurrent.*;
+class Calculation {
+   int z;
+	
+   public void addition(int x, int y) {
+      z = x + y;
+      System.out.println("The sum of the given numbers:"+z);
+   }
+	
+   public void Subtraction(int x, int y) {
+      z = x - y;
+      System.out.println("The difference between the given numbers:"+z);
+   }
+}
 
-  class DemoTask implements Runnable
-  {
-      private String name = null;
+ class My_Calculation extends Calculation {
+   public void multiplication(int x, int y) {
+      z = x * y;
+      System.out.println("The product of the given numbers:"+z);
+   }
+
+ }
+ 
+ class  StarWar{
    
-      public DemoTask(String name) {
-          this.name = name;
-      }
    
-      public String getName() {
-          return this.name;
-      }
-   
-      @Override
-      public void run() {
-          try {
-              Thread.sleep(500);
-          } catch (InterruptedException e) {
-              e.printStackTrace();
-          }
-          System.out.println("Executing : " + name);
-      }
-  }
-  
-  
-  
-  
-  class CustomThreadPoolExecutor extends ThreadPoolExecutor {
-	  
-	    public CustomThreadPoolExecutor(int corePoolSize, int maximumPoolSize,
-	            long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue) {
-	        super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue);
-	    }
-	 
-	    @Override
-	    protected void beforeExecute(Thread t, Runnable r) {
-	        super.beforeExecute(t, r);
-	        System.out.println("Perform beforeExecute() logic");
-	    }
-	 
-	    @Override
-	    protected void afterExecute(Runnable r, Throwable t) {
-	        super.afterExecute(r, t);
-	        if (t != null) {
-	            System.out.println("Perform exception handler logic");
-	        }
-	        System.out.println("Perform afterExecute() logic");
-	    }
-	 
-	}
-  
-  
-  
-  class  StarWar
-  {
-      public static void main(String[] args)
-      {
-          Integer threadCounter = 0;
-          BlockingQueue<Runnable> blockingQueue = new ArrayBlockingQueue<Runnable>(50);
-   
-          CustomThreadPoolExecutor executor = new CustomThreadPoolExecutor(10,
-                                              20, 5000, TimeUnit.MILLISECONDS, blockingQueue);
-   
-          executor.setRejectedExecutionHandler(new RejectedExecutionHandler() {
-              @Override
-              public void rejectedExecution(Runnable r,
-                      ThreadPoolExecutor executor) {
-                
-            	  System.out.println("DemoTask Rejected : "
-                          + ((DemoTask) r).getName());
-                  
-            	  System.out.println("Waiting for a second !!");
-                  try {
-                      Thread.sleep(1000);
-                  } catch (InterruptedException e) {
-                      e.printStackTrace();
-                  
-                  }
-                  
-                  System.out.println("Lets add another time : "
-                          + ((DemoTask) r).getName());
-                  executor.execute(r);
-              }
-          });
-          // Let start all core threads initially
-          executor.prestartAllCoreThreads();
-          while (true) {
-              threadCounter++;
-              // Adding threads one by one
-              System.out.println("Adding DemoTask : " + threadCounter);
-              executor.execute(new DemoTask(threadCounter.toString()));
-   
-              if (threadCounter == 100)
-                  break;
-          }
-      }
-   
-  }
-  
-  
-  
-  
+   public static void main(String args[]) {
+      int a = 20, b = 10;
+      My_Calculation demo = new My_Calculation();
+      demo.addition(a, b);
+      demo.Subtraction(a, b);
+      demo.multiplication(a, b);
+   }
+}
